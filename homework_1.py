b4f1e81ce684b7ec0aea8ebdcaa107f5e7882145
centr.yarik <centr.yarik@gmail.com>
2023-10-17 22:04:35 +0300

    HW 2 done

--- homework_1.py
+++ homework_1.py
@@ -1,4 +1,7 @@
 # Task 1
+print("HW1 TASK 1")
+
+
 class Product:
     """
     Defines a Product
@@ -43,6 +46,8 @@ print(cart.total_price())
 
 
 # Task 2
+print("HW1 TASK 2")
+
 
 class Dish:
     def __init__(self, name, description, price):
@@ -50,12 +55,27 @@ class Dish:
         self.description = description
         self.price = price
 
+    def __str__(self):
+        return f'{self.name}: {self.price}'
 
-class MenuCategory:
-    def __init__(self, name, *dishes):
-        self.category = {
-            name: dishes
-        }
+
+class MenuCategories:
+    def __init__(self):
+        self.menu = {}
+
+    def add_category(self, name):
+        if name not in self.menu:
+            self.menu[name] = set()
+
+    def add_dish(self, category, dish):
+        if category in self.menu:
+            self.menu[category].add(dish)
+
+    def __str__(self):
+        s = ''
+        for category, dishes in self.menu.items():
+            s += f'\n{category}:\n' + '\n'.join(map(str, dishes))
+        return s
 
 
 class Order:
@@ -78,9 +98,16 @@ pizza_4_cheese = Dish("pizza 4 cheese", "Tasty pizza with 4 cheeses", 40)
 soup = Dish("soup", "Tasty soup", 15)
 borshc = Dish("borshc", "Tasty borshc", 15)
 
-first_meal = MenuCategory("first meal", soup, borshc)
-second_meal = MenuCategory("second meal", spaghetti)
-third_meal = MenuCategory("third meal", pizza_4_cheese)
+menu = MenuCategories()
+
+menu.add_category("first meal")
+menu.add_category("second meal")
+menu.add_category("third meal")
+
+menu.add_dish("first meal", soup)
+menu.add_dish("first meal", borshc)
+menu.add_dish("second meal", spaghetti)
+menu.add_dish("third meal", pizza_4_cheese)
 
 order = Order()
 order.add_item(pizza_4_cheese)
@@ -90,3 +117,50 @@ order.add_item(soup)
 print(order.calculate_total())
 order.remove_item(borshc)
 print(order.calculate_total())
+print(menu)
+
+
+# Task HW2.1
+print("HW2 TASK 1")
+
+
+class Discount:
+    def __init__(self, disc):
+        self.disc = disc
+
+    def discount(self):
+        return 1 - self.disc
+
+
+class RegularDiscount(Discount):
+    def discount(self):
+        return 1 - self.disc
+
+
+class SilverDiscount(Discount):
+    def discount(self):
+        return 1 - self.disc
+
+
+class GoldDiscount(Discount):
+    def discount(self):
+        return 1 - self.disc
+
+
+class Client:
+    def __init__(self, name, discount: Discount):
+        self.name = name
+        self.discount = discount
+
+    def get_total_price(self, ordered: Order):
+        return ordered.calculate_total() * self.discount.discount()
+
+
+Silver = SilverDiscount(0.1)
+Gold = GoldDiscount(0.2)
+Regular = RegularDiscount(0.05)
+
+client1 = Client("Kolya", Silver)
+client2 = Client("Ivan", Gold)
+print(client1.get_total_price(order))
+print(client2.get_total_price(order))
